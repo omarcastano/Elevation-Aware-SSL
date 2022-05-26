9a4a0425f6c61587e849b2c431731328ba5d479a
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-05-25 22:12:22 -0500

    Exploratory Data Analysis
    Exploratory Data Analysis

--- EDA.py
+++ EDA.py
@@ -64,3 +64,214 @@ def visualize_image_bands(path_to_images, metadata):
     print("Image Shape (T,C,W,H):", image.shape)
     print('########################################')
     ep.plot_bands(image[0], cols=6, scale=True, cbar=False, figsize=(25,10));
+    
+    
+#This function plot pixel histogram for each band
+def pixel_histogram(path_to_images, metadata, sample=20, clip=None):
+
+    """
+    This function plots histogram and basic statistics for each band.
+    This function assums that images are stored in .npz format
+
+    Arguments:
+        path_to_images: string
+            path to the root folder where images are stroed
+        metadata: data frame
+            dataframe with the name of each image
+        sample: int
+            number of images to load and plot
+        clip: int, default=None
+            value to clip pixel images
+    """
+
+    images = np.array([read_numpy_image(img) for img in path_to_images + metadata.head(sample).Image])
+
+    if clip:
+        images = np.clip(images, 0, clip)
+
+
+    print("------Statistics for all Bands------")
+    print(f"Pixel Mean={images.mean().round(3)}")
+    print(f"Pixel Min={images.min()}")
+    print(f"Pixel Max={images.max()}")
+    print("------------------------------------")
+ 
+    
+    i = 1
+    fig, axs = plt.subplots(3,4, figsize=(30,10))
+    for img, ax in zip(images.transpose(2,0,1,3,4), axs.flatten()):
+        sns.histplot(x=img.ravel(), ax=ax)
+        ax.set_xlabel('Pixel Value')
+        ax.legend([f'Band {i} \n Mean={img.mean().round(3)} \n Min={img.min()}, \n Max={img.max()}'], fontsize=15)
+        i += 1
+    
+    sns.histplot(x=images.ravel(), ax=axs[2,3])
+    axs[2,3].set_xlabel('Pixel Value')
+    axs[2,3].legend(['All Bands'], fontsize=20)
+    
+    
+#Select the less cloudy image from the time series
+def less_cloudy_image(images):
+
+    """
+    This function select the less cloudy image from the time series
+
+    Arguments :
+        images (T,C,W,H):numpy array
+            Time series images
+    """
+    
+    means = images[:,0:3,:,:].mean(axis=(1,2,3))
+    idx = means[means > 0].argmin()
+
+    return images[idx]
+
+#Plot less cloudy image from the time seires
+def pixel_histogram_with_image(path_to_images, metadata, sample=5, scale_factor=13000, cloud_mask=False ,clip=None):
+
+    """
+    This function plots the histogram and the image for the RGB bands. You
+    can choose if select the less cloudy image from the time series or 
+    plot a random image from the it. Images are expected to have the 
+    shape (T,C,W,H), and RBG bands must be the firs three bands.
+
+    Arguments:
+        path_to_images: string
+            path to the root folder where images are stroed
+        metadata: data frame
+            dataframe with the name of each image
+        sample: int
+            number of images to load and plot
+        scale_factor: flaot
+            Factor to sacles images
+        clip: int, default=None
+            value to clip pixel images
+        cloud_mask: bool, default=True
+            wheter or not to select the less cloudy image
+        clip: int
+            upper bount value to clip images
+    """
+
+    if cloud_mask:
+        masked_images = []
+        images = np.array([read_numpy_image(img) for img in path_to_images + metadata.head(sample).Image])
+        for img in images:
+            masked_images.append(less_cloudy_image(img))
+        images = np.array(masked_images)
+    
+    else:
+        idx = 2#np.random.randint(0,24)
+        images = np.array([read_numpy_image(img)[idx] for img in path_to_images + metadata.head(sample).Image])
+
+    if clip:
+        images = np.clip(images, 0, clip)
+
+    print("------Statistics for the RGB bands------")
+    print(f"Pixel Mean={images[:,0:3,:,:].mean().round(3)}")
+    print(f"Pixel Min={images[:,0:3,:,:].min()}")
+    print(f"Pixel Max={images[:,0:3,:,:].max()}")
+    print("------------------------------------")
+
+    
+    i = 1
+    fig, ax = plt.subplots(2, sample, figsize=(30,10))
+
+    if scale_factor:
+        images = images[:,0:3,:,:]/scale_factor
+    else:
+        images = images[:,0:3,:,:]
+
+    for i, img in enumerate(images):
+
+        ax[0,i].imshow(img.transpose(1,2,0))
+        sns.histplot(x=img.ravel(), ax=ax[1,i])
+        ax[1,i].set_xlabel('Pixel Value')
+        ax[1,i].legend([f'Mean={img.mean().round(3)} \n Min={img.min().round(3)}, \n Max={img.max().round(3)}'], fontsize=10)
+        
+        
+        
+def simple_cloud_mask_filter(image, scale_factor, clip, threshold=0.5):
+
+    """
+    This function filter images based on a given threshold
+
+    Argumetns:
+        image (T,C,W,H):numpy array
+            Time series images
+        scale_factor: flaot
+            Factor to sacles images
+        clip: int
+            upper bount value to clip images
+        threshold: float
+            value to filter out cloudy images.
+    """
+
+    if clip:
+        image = np.clip(image, 0, clip)
+
+    if np.mean(image[:3]/scale_factor) < threshold:
+        return ("Not Remove", image)
+    else:
+        return ("Remove", image)
+
+
+def pixel_histogram_and_filtered_image(path_to_images, metadata, sample=5, scale_factor=13000, clip=None, threshold=0.5):
+
+    """
+    This function plots the histogram and the image for the RGB bands.
+    It also shows a label suggesting if an image from the time series
+    should be remove due to its high cloudiness. Images are expected 
+    to have the shape (T,C,W,H), and RBG bands must be the first
+    three bands.
+
+    Arguments:
+        path_to_images: string
+            path to the root folder where images are stroed
+        metadata: data frame
+            dataframe with the name of each image
+        sample: int
+            number of images from the time series to plot
+        scale_factor: flaot
+            Factor to sacles images
+        clip: int, default=None
+            value to clip pixel images
+        threshold: float
+            threshold to remove the image
+    """
+
+    masked_images = []
+    images = read_numpy_image(path_to_images + metadata.loc[2].Image)
+    for img in images:
+        masked_images.append(simple_cloud_mask_filter(img, scale_factor, clip, threshold))
+        
+    images = []
+    labels = []
+    for label, img in masked_images:
+        images.append(img)
+        labels.append(label)
+
+    images = np.array(images)
+
+    if clip:
+        images = np.clip(images, 0, clip)
+
+    print("------Statistics of RGB Bands------")
+    print(f"Pixel Mean={images[:,0:3,:,:].mean().round(3)}")
+    print(f"Pixel Min={images[:,0:3,:,:].min()}")
+    print(f"Pixel Max={images[:,0:3,:,:].max()}")
+    print("------------------------------------")
+    
+    i = 1
+    fig, ax = plt.subplots(2, sample, figsize=(60,10))
+    images = images[:,0:3,:,:]/scale_factor
+
+    for i, img in enumerate(images[0:sample]):
+        ax[0,i].imshow(img.transpose(1,2,0))
+        if labels[i] == 'Deprected':
+            ax[0,i].set_title(f'{labels[i]}', fontsize=20, color='r')
+        else: 
+            ax[0,i].set_title(f'{labels[i]}', fontsize=20)
+
+        sns.histplot(x=img.ravel(), ax=ax[1,i])
+        ax[1,i].set_xlabel('Pixel Value')
+        ax[1,i].legend([f'Mean={img.mean().round(3)} \n Min={img.min().round(3)}, \n Max={img.max().round(3)}'], fontsize=10)
