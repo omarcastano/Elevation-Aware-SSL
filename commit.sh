e5cdcbed309543319761c39649a6f2c2d8987fcc
omarcastano <omar.castano@udea.edu.co>
2022-06-02 03:26:04 +0000

    Make script.sh executable.

--- commit.sh
+++ commit.sh
@@ -1,6 +1,8 @@
+#make this file execuatble with chmod +x <fileName>
 git config --global user.email "omar.castano@udea.edu.co"
 git config --global user.name "omarcastano"
-
-git add .
-git commit -m "changes"
-git push
+#git status
+#git add .
+#git commit -m "changes"
+#git push
+#git status
