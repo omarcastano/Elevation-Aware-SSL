a4e9255e49df872606af0c76534ab9fa8547d970
omarcastano <omar.castano@udea.edu.co>
2022-06-02 03:27:23 +0000

    changes

--- commit.sh
+++ commit.sh
@@ -1,8 +1,3 @@
 #make this file execuatble with chmod +x <fileName>
 git config --global user.email "omar.castano@udea.edu.co"
 git config --global user.name "omarcastano"
-#git status
-#git add .
-#git commit -m "changes"
-#git push
-#git status
