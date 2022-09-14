6f2687c972c316dda707ffc15aee55da63e6c51a
omarcastano <omar.castano@udea.edu.co>
2022-09-14 16:40:52 -0500

    changes

--- MasterThesis/utils/classification/metrics.py
+++ MasterThesis/utils/classification/metrics.py
@@ -178,7 +178,7 @@ class threshold_metric_evaluation:
 
         for y in np.unique(y_true):
             y_pred_proba = y_score[:, y].ravel()
-            for t in np.arange(0.00, 1.0, 0.1):
+            for t in np.arange(0.00, 0.99, 0.1):
 
                 y_pred = (y_pred_proba >= t).astype(int)
 
