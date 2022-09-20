8dfbe99ddb38955a15c32296de99c5d8077d7ef7
omarcastano <omar.castano@udea.edu.co>
2022-09-19 23:22:38 -0500

    some changes

--- MasterThesis/utils/classification/simclr.py
+++ MasterThesis/utils/classification/simclr.py
@@ -46,7 +46,7 @@ def data_augmentation(img, min_max_croped_size):
             transforms.RandomResizedCrop(size=img.shape[1], scale=(0.9, 1.0)),
             transforms.RandomApply([transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1)], p=0.8),
             transforms.RandomGrayscale(p=0.5),
-            transforms.GaussianBlur(kernel_size=5),
+            # transforms.GaussianBlur(kernel_size=5),
         ]
     )
 
