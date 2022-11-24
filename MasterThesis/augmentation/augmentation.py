a2d165982d57a4541fc9e2baddeed9682f808980
omarcastano <omar.castano@udea.edu.co>
2022-11-24 18:34:05 -0500

    add agumentation-v2

--- MasterThesis/augmentation/augmentation.py
+++ MasterThesis/augmentation/augmentation.py
@@ -2,7 +2,10 @@
 This Module provides functions to apply data augmentation 
 """
 
+import torch
 from torchvision import transforms
+import albumentations as album
+import numpy as np
 
 AUGMENTATIONS = {
     "horizontal_flip_prob": 0.5,
@@ -51,6 +54,57 @@ def data_augmentation(img, augment: dict = None):
         ]
     )
 
+    img = torch.from_numpy(img.astype(np.float32))
     img = augmentation(img)
 
     return img
+
+
+AUGMENTATIONS_V2 = {
+    "horizontal_flip_prob": 0.5,
+    "vertical_flip_prob": 0.5,
+    "resize_scale": (0.85, 1.0),
+    "resize_prob": 0.5,
+    "brightness": 0.2,
+    "contrast": 0.2,
+    "saturation": 0.2,
+    "hue": 0.1,
+    "color_jitter_prob": 0.5,
+    "gray_scale_prob": 0.2,
+}
+
+
+def data_augmentation_v2(img, label=None, augment: dict = None):
+
+    augment = augment or AUGMENTATIONS_V2
+
+    # Defines transformation
+    augmentation = album.Compose(
+        [
+            album.VerticalFlip(p=augment["vertical_flip_prob"]),
+            album.HorizontalFlip(p=augment["horizontal_flip_prob"]),
+            album.RandomResizedCrop(height=img.shape[1], width=img.shape[2], scale=augment["resize_scale"], p=augment["resize_prob"]),
+            album.ColorJitter(
+                brightness=augment["brightness"],
+                contrast=augment["contrast"],
+                saturation=augment["saturation"],
+                hue=augment["hue"],
+                p=augment["color_jitter_prob"],
+            ),
+            album.ToGray(p=augment["gray_scale_prob"]),
+        ]
+    )
+
+    img = img.transpose(1, 2, 0).astype(np.float32)
+
+    if label:
+        augmented = augmentation(image=img, mask=label)
+        img, label = augmented["image"].transpose(2, 0, 1), augmented["mask"]
+
+        return img, label
+
+    else:
+        augmented = augmentation(image=img)
+        img = augmented["image"].transpose(2, 0, 1)
+
+        return img
